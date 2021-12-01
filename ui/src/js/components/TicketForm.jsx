ebb82ad4382128f3ce6f841788a23af3c0432b9e
ashleyh <ashleyhellier17@gmail.com>
2021-12-01 02:41:00 -0800

    cinema form commited for standard and extras, eslint, nix and prettier added

---
+++ ui/src/js/components/TicketForm.jsx
@@ -0,0 +1,118 @@
+import { React, useState } from 'react';
+import { Button } from 'react-bootstrap';
+import ExtraForm from './ExtraForm';
+
+
+function TicketForm() {
+
+    const [ standardTicketCount, setStandardTicketCount ] = useState(0);
+    const [ concessionTicketCount, setConcessionTicketCount ] = useState(0);
+    const [ standardTotalPrice, setStandardTotalPrice ] = useState(0);
+    const [ concessionTotalPrice, setConcessionTotalPrice ] = useState(0);
+    const [ totalPrice, setTotalPrice ] = useState(0);
+    const [ standard3DCount, setStandard3DCount ] = useState(0);
+    const [ concession3DCount, setConcession3DCount ] = useState(0);
+    const [ standardIMAXCount, setStandardIMAXCount ] = useState(0);
+    const [ concessionIMAXCount, setConcessionIMAXCount ] = useState(0);
+    const [ updatedTotalPrice, setUpdatedTotalPrice ] = useState(0);
+    const [ savingAmount, setSavingAmount ] = useState(0);
+
+    const increaseStandardTicketCount = () => {
+        setStandardTicketCount(prevstandardTicketCount => prevstandardTicketCount + 1);
+    }
+
+    const decreaseStandardTicketCount = () => {
+        setStandardTicketCount(prevstandardTicketCount => prevstandardTicketCount - 1);
+    }
+
+    const increaseConcessionTicketCount = () => {
+        setConcessionTicketCount(prevconcessionTicketCount => prevconcessionTicketCount + 1);
+    }
+
+    const decreaseConcessionTicketCount = () => {
+        setConcessionTicketCount(prevconcessionTicketCount => prevconcessionTicketCount - 1);
+    }
+
+    const increaseStandard3DTicketCount = () => {
+        setStandard3DCount(prevstandard3DCount => prevstandard3DCount + 1);
+    }
+
+    const decreaseStandard3DTicketCount = () => {
+        setStandard3DCount(prevstandard3DCount => prevstandard3DCount - 1);
+    }
+
+    const increaseConcession3DTicketCount = () => {
+        setConcession3DCount(prevconcession3DCount => prevconcession3DCount + 1);
+    }
+
+    const decreaseConcession3DTicketCount = () => {
+        setConcession3DCount(prevconcession3DCount => prevconcession3DCount - 1);
+    }
+
+    const increaseStandardIMAXTicketCount = () => {
+        setStandardIMAXCount(prevstandardIMAXCount => prevstandardIMAXCount + 1);
+    }
+
+    const decreaseStandardIMAXTicketCount = () => {
+        setStandardIMAXCount(prevstandardIMAXCount => prevstandardIMAXCount - 1);
+    }
+
+    const increaseConcessionIMAXTicketCount = () => {
+        setConcessionIMAXCount(prevconcessionIMAXCount => prevconcessionIMAXCount + 1);
+    }
+
+    const decreaseConcessionIMAXTicketCount = () => {
+        setConcessionIMAXCount(prevconcessionIMAXCount => prevconcessionIMAXCount - 1);
+    }
+
+    return(
+
+        <div>
+            <div>
+                <div>
+                    <p>Standard Ticket:</p>{' '}
+                        <Button onClick={increaseStandardTicketCount} variant="primary">+</Button>{' '}
+                        <Button onClick={decreaseStandardTicketCount} variant="primary">-</Button>{' '}
+                </div>
+                <div>Standard Ticket Total: {standardTicketCount}</div>
+                <br/>
+                <div>
+                    <p>Concession Ticket:</p>{' '}
+                        <Button onClick={increaseConcessionTicketCount} variant="primary">+</Button>{' '}
+                        <Button onClick={decreaseConcessionTicketCount} variant="primary">-</Button>{' '}
+
+                </div>
+                <div>Concession Ticket Total: {concessionTicketCount}</div>
+            </div>
+            <br/>
+            <br/>
+            <br/>
+            <div>
+                <ExtraForm standard3DCount={standard3DCount}
+                            concession3DCount={concession3DCount}
+                            standardIMAXCount={standardIMAXCount}
+                            concessionIMAXCount={concessionIMAXCount}
+                            increaseStandard3DTicketCount={increaseStandard3DTicketCount}
+                            decreaseStandard3DTicketCount={decreaseStandard3DTicketCount}
+                            increaseConcession3DTicketCount={increaseConcession3DTicketCount}
+                            decreaseConcession3DTicketCount={decreaseConcession3DTicketCount}
+                            increaseStandardIMAXTicketCount={increaseStandardIMAXTicketCount}
+                            decreaseStandardIMAXTicketCount={decreaseStandardIMAXTicketCount}
+                            increaseConcessionIMAXTicketCount={increaseConcessionIMAXTicketCount}
+                            decreaseConcessionIMAXTicketCount={decreaseConcessionIMAXTicketCount}
+                            standardTotalPrice={standardTotalPrice}
+                            concessionTotalPrice={concessionTotalPrice}
+                            setStandardTotalPrice={setStandardTotalPrice}
+                            setConcessionTotalPrice={setConcessionTotalPrice}
+                            setTotalPrice={setTotalPrice}>
+                            </ExtraForm>
+                            <br/>
+                </div>
+            </div>
+        
+
+
+    )
+}
+
+export default TicketForm;
---
+++ ui/src/js/pages/Home.jsx
@@ -0,0 +1,19 @@
+import { React } from 'react';
+import TicketForm from '../components/TicketForm';
+
+function Home() {
+  return (
+    <div>
+      <h3>Welcome to the Cinema!</h3>
+      <p>
+        Please select your tickets from the options below and then click to get
+        your exclusive Thursday offer!
+      </p>
+      <br />
+      <TicketForm/>
+
+    </div>
+  );
+}
+
+export default Home;
